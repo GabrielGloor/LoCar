003123975c98239f85c812a461209acc03e7f232
HUMBERT-DROZ-LAURENT Romain <Romain.HUMBERT-DROZ-LAURENT@cpnv.ch>
2022-02-10 10:12:22 +0100

    Add controller for the navigation

--- controller/nav.php
+++ controller/nav.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * @Author :
- * @Date :
- * @Version :
- * @Description :
+ * @Author : ROmain Humbert-Droz-Laurent
+ * @Date : 10.02.2022
+ * @Version : 0.1
+ * @Description : This file is designed to show a page to the user.
  */
 
+function home()
+{
+    require "view/home.php";
+}
 ?>
