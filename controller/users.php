a525ce24e1a8baf6a922e024c50c9586eb80b446
HUMBERT-DROZ-LAURENT Romain <Romain.HUMBERT-DROZ-LAURENT@cpnv.ch>
2022-02-10 10:12:56 +0100

    Add controller for the users' input

--- controller/users.php
+++ controller/users.php
@@ -1,9 +1,19 @@
 <?php
 /**
- * @Author :
- * @Date :
- * @Version :
- * @Description :
+ * @Author : Romain Humbert-Droz-Laurent
+ * @Date : 10.02.2022
+ * @Version : 0.1
+ * @Description : This file is designed to verify for the login and the register if the user input are filled or not.
  */
 
-?>
+function login($inputs)
+{
+    if (isset($inputs['inputEmail']) && isset($inputs['inputPswd']))
+    {
+
+    }
+    else
+    {
+        require "view/login.php";
+    }
+}
