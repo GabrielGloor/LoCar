fc2758a8cdda4389e5f8fc8f19a0e2a906367a07
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-02-11 14:24:02 +0100

    Ended the register function

--- controller/users.php
+++ controller/users.php
@@ -6,15 +6,16 @@
  * @Description : This file is designed to verify for the login and the register if the user input are filled or not.
  */
 
+require "model/usersManager.php";
+
 /**
  * @brief   This function is designed to verify if the requirements datas are filled
- * @param $inputs Is the logs of the users
+ * @param $inputs : Is the logs of the users
  */
 function login($inputs)
 {
     if (isset($inputs['inputEmail']) && isset($inputs['inputPswd']))
     {
-        require "model/usersManager.php";
         isUserCorrect($inputs['inputEmail'],$inputs['inputPswd']);
     }
     else
@@ -25,10 +26,19 @@ function login($inputs)
 
 /**
  * @brief This function is designed to verify if the inputs are filled. If not it will show the register page, else it will register the user. (WIP)
+ * @param $inputs : It is the user input in the register form
  */
-function register()
+function register($inputs)
 {
-    require "view/register.php";
+    if (isset($inputs['inputUsername']) && isset($inputs['inputEmail']) && isset($inputs['inputPswd']) && isset($inputs['inputPswdTwo'])){
+        if ($inputs['inputPswd'] == $inputs['inputPswdTwo']){
+            userRegister($inputs['inputUsername'], $inputs['inputEmail'], $inputs['inputPswd']);
+        }else{
+            header('Location: ?action=register&incorrect=true&badPassword=true');
+        }
+    }else{
+        require "view/register.php";
+    }
 }
 
 /**
