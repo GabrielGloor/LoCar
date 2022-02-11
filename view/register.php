45dab733756442c3143b099a3761ccd4195cd89e
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-02-11 14:26:16 +0100

    Updated return messages

--- view/register.php
+++ view/register.php
@@ -38,7 +38,7 @@
                     LoCar - Inscription
                 </span>
 
-                <div class="wrap-input100 validate-input">
+                <div class="wrap-input100 validate-input" data-validate = "Vous devez rentrer un nom d'utilisateur">
                     <input class="input100" type="text" name="inputUsername">
                     <span class="focus-input100"></span>
                     <span class="label-input100">Nom d'utilisateur</span>
@@ -56,6 +56,12 @@
                     <span class="label-input100">Mot de passe</span>
                 </div>
 
+                <div class="wrap-input100 validate-input" data-validate="Vous devez entrer le même mot de passe">
+                    <input class="input100" type="password" name="inputPswdTwo" id="psswd2">
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Confirmer le mot de passe</span>
+                </div>
+
                 <div class="flex-sb-m w-full p-t-3 p-b-32">
                     <div class="contact100-form-checkbox">
                         <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" onchange="fn_changePsswdView(this.id)">
@@ -64,6 +70,22 @@
                         </label>
                     </div>
                 </div>
+
+                <?php
+                if(isset($_GET['incorrect']) && isset($_GET['badPassword'])){
+                    if($_GET['incorrect'] == 'true' && $_GET['badPassword'] == 'true'){
+                        echo "<div class='registerErr'>
+                                <p style='color: red; margin-top: -15px; margin-bottom: 15px;'>Votre mot de passe ne correpond pas à la confirmation !</p>
+                              </div>";
+                    }
+                }elseif(isset($_GET['incorrect']) && isset($_GET['userExists'])){
+                    if($_GET['incorrect'] == 'true' && $_GET['userExists'] == 'true'){
+                        echo "<div class='registerErr'>
+                                <p style='color: red; margin-top: -15px; margin-bottom: 15px;'>Le nom d'utilisateur et/ou l'email est déjà utilisé !</p>
+                              </div>";
+                    }
+                }
+                ?>
 
                 <div class="container-login100-form-btn">
                     <button class="login100-form-btn">
