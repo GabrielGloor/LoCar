c1ed9bef177b47e76e1eb770253c68488d59dc7f
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-02-11 14:24:23 +0100

    Modified the register action

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (isset($_GET['action'])) {
             logout();
             break;
         case 'register':
-            register();
+            register($_POST);
             break;
         default :
             lost();
