06a89ee19bbe154bf4e30b91ab8b8970ea5c1aa8
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-02-11 08:56:25 +0100

    Added the lost page

---
+++ view/lost.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file         lost.php
+ * @author       diogo.da-silva-fernandes@cpnv.ch
+ * @date         11.02.2022
+ * @project      LoCar
+ * @description  Lost page
+ * @last_update  diogo.da-silva-fernandes@cpnv.ch - 11.02.2022
+ */
+ob_start();
+
+$title = "Vous vous êtes perdu !";
+
+require "view/contents/top_bar.php";
+?>
+
+<div style="padding: 300px 0px">
+    <h1 style="text-align: center">Je crois que vous vous êtes perdu :(</h1>
+</div>
+<?php
+$content = ob_get_clean();
+require 'gabarit.php'
+?>
