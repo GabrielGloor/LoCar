d5ba61cb7762e3b87a9001a929918fc6b01a349d
DA-SILVA-FERNANDES Diogo <Diogo.da-silva-fernandes@cpnv.ch>
2022-02-11 08:32:17 +0100

    Modifications and debug login page

--- view/login.php
+++ view/login.php
@@ -16,7 +16,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<title>Login | LoCar</title>
+	<title>LoCar - Connexion</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -33,18 +33,18 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form" action="?action=loginSend" method="post">
+				<form class="login100-form validate-form" action="?action=login" method="post">
 					<span class="login100-form-title p-b-43">
 						LoCar - Connexion
 					</span>
 					
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate = "Email valide: ex@abc.xyz">
 						<input class="input100" type="text" name="inputEmail">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Email</span>
 					</div>
 					
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Vous devez entrer un mot de passe">
 						<input class="input100" type="password" name="inputPswd" id="psswd">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Mot de passe</span>
