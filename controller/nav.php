7c3a1115ba5ee69a845e0b4dbdd0d3bf5ef63757
HUMBERT-DROZ-LAURENT Romain <romain.humbert-droz-laurent@cpnv.ch>
2022-02-11 08:29:06 +0100

    Add comments

--- controller/nav.php
+++ controller/nav.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * @Author : ROmain Humbert-Droz-Laurent
+ * @Author : Romain Humbert-Droz-Laurent
  * @Date : 10.02.2022
  * @Version : 0.1
  * @Description : This file is designed to show a page to the user.
  */
 
+/**
+ * @brief This function is designed to show the home page
+ */
 function home()
 {
     require "view/home.php";
