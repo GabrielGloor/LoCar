1972dc98923d4b8a80ccaea1bbef2c6c42b80680
HUMBERT-DROZ-LAURENT Romain <romain.humbert-droz-laurent@cpnv.ch>
2022-02-11 08:29:27 +0100

    Debug and add comments

--- controller/users.php
+++ controller/users.php
@@ -6,11 +6,16 @@
  * @Description : This file is designed to verify for the login and the register if the user input are filled or not.
  */
 
+/**
+ * @brief   This function is designed to verify if the requirements datas are filled
+ * @param $inputs Is the logs of the users
+ */
 function login($inputs)
 {
     if (isset($inputs['inputEmail']) && isset($inputs['inputPswd']))
     {
-
+        require "model/usersManager.php";
+        isUserCorrect($inputs['inputEmail'],$inputs['inputPswd']);
     }
     else
     {
