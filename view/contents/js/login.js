8fbd6e8a4dfdb2950da709e59528a3edde7dba8b
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-02-11 10:06:18 +0100

    Debug for the email check

--- view/contents/js/login.js
+++ view/contents/js/login.js
@@ -51,7 +51,7 @@ function fn_changePsswdView(itemId){
     });
 
     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'inputEmail') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
