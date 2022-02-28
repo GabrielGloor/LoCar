773901b9bbb0b615a67383c9b3b7fbaecef80b76
HUMBERT-DROZ-LAURENT Romain <Romain.HUMBERT-DROZ-LAURENT@cpnv.ch>
2022-02-28 11:08:59 +0100

    Update function to get offers Infos and move the show offers function to the controller

--- model/offersManagement.php
+++ model/offersManagement.php
@@ -8,57 +8,10 @@
 
 require_once "model/json.php";
 
-/**
- * @brief This function is designed to show all offers
- */
-function showOffers(){
-
-    $offersData = decodeJson("model/content/offers.json");
-
-    $nbOffers = count($offersData);
-    if ($nbOffers == 0){
-        $err = "Il n'y a aucune offre actuellement";
-        require "view/contents/offer_template.php";
-    }else{
-        for ($showCurrentOffers = 0; $showCurrentOffers < $nbOffers; $showCurrentOffers++) {
-            $class = $showCurrentOffers%2 ? $showCurrentOffers." pair" : $showCurrentOffers." impair";
-            $name = $offersData[$showCurrentOffers]['name'];
-            $linkImg = $offersData[$showCurrentOffers]['image'];
-            $linkToDetails = "?action=offerDetails&offerId=".$offersData[$showCurrentOffers]['id'];
-            $price = $offersData[$showCurrentOffers]['price'];
-
-            require "view/contents/offer_template.php";
-        }
-    }
-}
-
-
-/**
- * @brief This function is designed to show the details of one offer
- * @param $offerId : This param is for designate only one offer
- */
-function showOffersDetails($offerId){
-    $offersData = decodeJson("model/content/offers.json");
-
-    $name = null;
-    $linkImg = null;
-    $price = null;
-    $brand = null;
-    $year = null;
-    $description = null;
-    $town = null;
-
-    foreach ($offersData as $offer){
-        if ($offer['id'] == $offerId) {
-            $name = $offersData['name'];
-            $linkImg = $offersData['image'];
-            $price = $offersData['price'];
-            $description = $offersData['description'];
-            $brand = $offersData['brand'];
-            $year = $offersData['year'];
-            $town = $offersData['town'];
-        }
-    }
+function getOffersInfos(){
+    $jsonFile = "model/content/offers.json";
+    $offersInfos = decodeJson($jsonFile);
+    return $offersInfos;
 }
 
 /**
