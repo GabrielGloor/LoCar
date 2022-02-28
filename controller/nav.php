e8034c2c928b081ac44c493a6f704813728caca2
HUMBERT-DROZ-LAURENT Romain <Romain.HUMBERT-DROZ-LAURENT@cpnv.ch>
2022-02-28 11:07:06 +0100

    Move the offers function to another file

--- controller/nav.php
+++ controller/nav.php
@@ -23,13 +23,6 @@ function lost()
 }
 
 /**
- * @brief This function is designed to show the offers page
- */
-function offers(){
-    require "view/offers.php";
-}
-
-/**
  * @brief This function is designed to show the offer details page
  */
 function offerDetails(){
