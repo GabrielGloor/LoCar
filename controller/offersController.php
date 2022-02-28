661e431b156c5f5769313255e727d0504e8f5774
HUMBERT-DROZ-LAURENT Romain <Romain.HUMBERT-DROZ-LAURENT@cpnv.ch>
2022-02-28 11:07:50 +0100

    Create a controller for the offers

--- controller/offersController.php
+++ controller/offersController.php
@@ -1,9 +1,35 @@
 <?php
 /**
- * @Author :
- * @Date :
- * @Version :
- * @Description :
+ * @Author : Romain Humbert-Droz-Laurent
+ * @Date : 28.01.2022
+ * @Version : 0.1
+ * @Description : This file is designed to manage the offers
  */
 
-?>
+function offers(){
+    require "view/offers.php";
+}
+
+/**
+ * @brief This function is designed to show all offers
+ */
+function showOffers(){
+
+    $offersData = getOffersInfos();
+
+    $nbOffers = count($offersData);
+    if ($nbOffers == 0){
+        $err = "Il n'y a aucune offre actuellement";
+        require "view/contents/offer_template.php";
+    }else{
+        for ($showCurrentOffers = 0; $showCurrentOffers < $nbOffers; $showCurrentOffers++) {
+            $class = $showCurrentOffers%2 ? $showCurrentOffers." pair" : $showCurrentOffers." impair";
+            $name = $offersData[$showCurrentOffers]['name'];
+            $linkImg = $offersData[$showCurrentOffers]['image'];
+            $linkToDetails = "?action=offerDetails&offerId=".$offersData[$showCurrentOffers]['id'];
+            $price = $offersData[$showCurrentOffers]['price'];
+
+            require "view/contents/offer_template.php";
+        }
+    }
+}
