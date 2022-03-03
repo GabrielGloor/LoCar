3f94083e84e91f04a75cf8320b8361296e5cafc9
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-03-03 10:08:58 +0100

    Finished the first version of the offer details page

--- view/offerDetails.php
+++ view/offerDetails.php
@@ -26,7 +26,61 @@ require 'view/contents/top_bar.php';
     <hr>
 
     <div class="infos">
-        <p><span style="font-weight: bold">Description :</span> aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <div class="brand">
+            <p><span style="font-weight: bold">Marque :</span> Audi</p>
+        </div>
+        <div class="year">
+            <p><span style="font-weight: bold">Année :</span> 2022</p>
+        </div>
+        <div class="desc">
+            <p><span style="font-weight: bold">Description :</span></p>
+            <textarea readonly rows="10">asddddddddddddddddddddddddddddddddddddddddddddddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</textarea>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="contact">
+        <section class="contact_section" id="nous-contacter" style="margin-top: -70px">
+            <div class="container">
+                <div class="heading_container d-flex justify-content-center" style="margin-top: 120px; margin-bottom: -80px">
+                    <h3>
+                        Contacter le propriétaire
+                    </h3>
+                </div>
+                <div class="layout_padding">
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-md-6">
+                            <form action="#" method="post">
+                                <div class="contact_form-container">
+                                    <div>
+                                        <div>
+                                            <input type="text" placeholder="Nom">
+                                        </div>
+                                        <div>
+                                            <input type="text" placeholder="Numéro de téléphone">
+                                        </div>
+                                        <div>
+                                            <input type="email" placeholder="Email">
+                                        </div>
+                                        <div class="mt-5">
+                                            <textarea name="msg" cols="30" rows="10" placeholder="Message"></textarea>
+                                        </div>
+                                        <div class="mt-5 d-flex justify-content-center justify-content-sm-start">
+                                            <button type="submit">
+                                                Envoyer
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 <?php
