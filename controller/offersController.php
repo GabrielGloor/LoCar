3cae8d5984a9028ed01a1a6f82388b15a982dd96
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-03-03 10:08:20 +0100

    Modified functions and added the function to show the offers in the home page

--- controller/offersController.php
+++ controller/offersController.php
@@ -6,6 +6,8 @@
  * @Description : This file is designed to manage the offers
  */
 
+require "model/offersManagement.php";
+
 function offers(){
     require "view/offers.php";
 }
@@ -14,7 +16,6 @@ function offers(){
  * @brief This function is designed to show all offers
  */
 function showOffers(){
-
     $offersData = getOffersInfos();
 
     $nbOffers = count($offersData);
@@ -23,6 +24,29 @@ function showOffers(){
         require "view/contents/offer_template.php";
     }else{
         for ($showCurrentOffers = 0; $showCurrentOffers < $nbOffers; $showCurrentOffers++) {
+            $class = $showCurrentOffers%2 ? $showCurrentOffers." pair" : $showCurrentOffers." impair";
+            $name = $offersData[$showCurrentOffers]['name'];
+            $linkImg = $offersData[$showCurrentOffers]['image'];
+            $linkToDetails = "?action=offerDetails&offerId=".$offersData[$showCurrentOffers]['id'];
+            $price = $offersData[$showCurrentOffers]['price'];
+
+            require "view/contents/offer_template.php";
+        }
+    }
+}
+
+/**
+ * @brief This function is designed the last 4 offers only in the home page
+ */
+function showOffersInHomePage(){
+    $offersData = getOffersInfos();
+
+    $nbOffers = count($offersData);
+    if ($nbOffers == 0){
+        $err = "Il n'y a aucune offre actuellement";
+        require "view/contents/offer_template.php";
+    }else{
+        for ($showCurrentOffers = $nbOffers - 4;    $showCurrentOffers < $nbOffers; $showCurrentOffers++) {
             $class = $showCurrentOffers%2 ? $showCurrentOffers." pair" : $showCurrentOffers." impair";
             $name = $offersData[$showCurrentOffers]['name'];
             $linkImg = $offersData[$showCurrentOffers]['image'];
