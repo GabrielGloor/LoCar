e2e22e76833f8a667bb4f0b08edf648757ca90df
Diogo da Silva Fernandes <diogo.da-silva-fernandes@cpnv.ch>
2022-04-06 14:58:54 +0200

    Added confirmation for mails

--- view/offerDetails.php
+++ view/offerDetails.php
@@ -51,20 +51,21 @@ require 'view/contents/top_bar.php';
                 <div class="layout_padding">
                     <div class="row d-flex justify-content-center">
                         <div class="col-md-6">
-                            <form action="#" method="post">
+                            <form action="?action=email&offerId=<?=$_GET['offerId']?>" method="post">
                                 <div class="contact_form-container">
                                     <div>
+                                        <input type="hidden" name="ownerEmail" value="<?=$ownerEmail?>" required>
                                         <div>
-                                            <input type="text" placeholder="Nom">
+                                            <input type="text" name="name" placeholder="Nom" required>
                                         </div>
                                         <div>
-                                            <input type="text" placeholder="Numéro de téléphone">
+                                            <input type="text" name="telNumber" placeholder="Numéro de téléphone" required pattern="[0-9]{10}">
                                         </div>
                                         <div>
-                                            <input type="email" placeholder="Email">
+                                            <input type="email" name="mail" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
                                         </div>
                                         <div class="mt-5">
-                                            <textarea name="msg" cols="30" rows="10" placeholder="Message"></textarea>
+                                            <textarea name="msg" cols="30" rows="10" placeholder="Message" name="msg"></textarea>
                                         </div>
                                         <div class="mt-5 d-flex justify-content-center justify-content-sm-start">
                                             <button type="submit" style="width: 90%">
@@ -72,7 +73,17 @@ require 'view/contents/top_bar.php';
                                             </button>
                                         </div>
                                     </div>
-
+                                    <?php
+                                        if(isset($_GET['confirm']) && $_GET['confirm'] == 'true'){
+                                    ?>
+                                        <p style="color: green; text-align: center; margin-left: -40px; margin-top: 20px">Votre email a bien été transmis au propriétaire du véhicule. Vous avez également reçu un email de confirmation.</p>
+                                    <?php 
+                                        }elseif(isset($_GET['confirm']) && $_GET['confirm'] == 'false'){
+                                    ?>
+                                        <p style="color: red; text-align: center; margin-left: -40px; margin-top: 20px">Votre email n'a pas pu être livré. Veuillez réessayer plus tard.</p>
+                                    <?php
+                                        }
+                                    ?>
                                 </div>
 
                             </form>
