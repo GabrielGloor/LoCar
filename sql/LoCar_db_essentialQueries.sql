b607cdbe1a7f2221d1ec38920521472d457ce58a
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-06-02 08:08:57 +0200

    Update the essential queries

--- sql/LoCar_db_essentialQueries.sql
+++ sql/LoCar_db_essentialQueries.sql
@@ -2,32 +2,28 @@
  * Projet  : LoCar
  * Title   : db_essentialQueries
  * Author  : Diogo da Silva Fernandes
- * Version : 1.0
+ * Version : 1.1
  */
 
-/* Users table */
+/* Select the user infos */
 SELECT username, email, password FROM users;
 
-INSERT INTO users(username,email,password) VALUES ("USERNAME","EMAIL","PASSWORD");
+/* Create a new user */
+INSERT INTO users(username,email,password) VALUES ("diogof648","diogof648@gmail.com","Pa$$w0rd");
 
-UPDATE users SET username = "USERNAME", email = "EMAIL", password = "PASSWORD" WHERE username = "USERNAME";
+/* Update the user infos */
+UPDATE users SET username = "diogof647", email = "diogof647@gmail.com", password = "Password" WHERE username = "diogof648";
 
-DELETE FROM users WHERE username = "USERNAME";
+/* Select the offer ifnos */
+SELECT id, title, description, price, town, brand, year, image, user_id FROM offers;
 
-/* Offers table */
-SELECT offerNumber, title, description, price, town, brand, year, image, user_id FROM offers;
+/* Create a new offer */
+INSERT INTO offers(title, description, price, town, brand, year, image, user_id) VALUES ("Audi A8", "Magnifique audi A8", 192.55, "Yverdon", "Audi", 2003, "img/offer_img/audi_a8.png", 2);
 
-INSERT INTO offers(offerNumber, title, description, price, town, brand, year, image, user_id) VALUES (XXXXXXX, "TITLE", "DESCRIPTION", XXXXX.XX, "TOWN", "BRAND", XXXX, "IMAGE", X);
-
-UPDATE offers SET title = "TITLE", description = "DESCRITPION", price = XXXXX.XX, town = "TOWN", brand = "BRAND", year = XXXX, image = "IMAGE", user_id = X WHERE offerNumber = XXXXXXX;
-
-DELETE FROM offers WHERE offerNumber = XXXXXXX;
+/* Update the offer infos */
+UPDATE offers SET title = "Audi A7", description = "Magnifique audi A7", price = 92,55, town = "Yverdon-Les-Bains", brand = "AAudi", year = 2004, image = "img/offer_img/audi_a7.png", user_id = 2 WHERE id = 192178;
 
 /* Get the email of the owner of an offer */
 SELECT users.email, offers.offerNumber FROM users
 INNER JOIN offers ON users.id = offers.user_id
-WHERE offers.offerNumber = XXXXXXX;
-
-/*
- * /!\ Notice: Replace X with numbers /!\
- */
+WHERE offers.offerNumber = 192178;
