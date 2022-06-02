a439dd1c05c1bf8386c815a00b414fd325782e52
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-06-02 08:34:43 +0200

    Updated essential queries

--- sql/LoCar_db_essentialQueries.sql
+++ sql/LoCar_db_essentialQueries.sql
@@ -2,7 +2,7 @@
  * Projet  : LoCar
  * Title   : db_essentialQueries
  * Author  : Diogo da Silva Fernandes
- * Version : 1.1
+ * Version : 1.2
  */
 
 /* Select the user infos */
@@ -14,16 +14,13 @@ INSERT INTO users(username,email,password) VALUES ("diogof648","diogof648@gmail.
 /* Update the user infos */
 UPDATE users SET username = "diogof647", email = "diogof647@gmail.com", password = "Password" WHERE username = "diogof648";
 
-/* Select the offer ifnos */
-SELECT id, title, description, price, town, brand, year, image, user_id FROM offers;
+/* Select the offer infos */
+SELECT offers.id, offers.title, offers.description, offers.price, offers.town, offers.brand, offers.year, offers.image, users.email FROM users
+INNER JOIN offers ON users.id = offers.user_id
+WHERE offers.id = 192168;
 
 /* Create a new offer */
 INSERT INTO offers(title, description, price, town, brand, year, image, user_id) VALUES ("Audi A8", "Magnifique audi A8", 192.55, "Yverdon", "Audi", 2003, "img/offer_img/audi_a8.png", 2);
 
 /* Update the offer infos */
-UPDATE offers SET title = "Audi A7", description = "Magnifique audi A7", price = 92,55, town = "Yverdon-Les-Bains", brand = "AAudi", year = 2004, image = "img/offer_img/audi_a7.png", user_id = 2 WHERE id = 192178;
-
-/* Get the email of the owner of an offer */
-SELECT users.email, offers.offerNumber FROM users
-INNER JOIN offers ON users.id = offers.user_id
-WHERE offers.offerNumber = 192178;
+UPDATE offers SET title = "Audi A7", description = "Magnifique audi A7", price = 92.55, town = "Yverdon-Les-Bains", brand = "AAudi", year = 2004, image = "img/offer_img/audi_a7.png", user_id = 2 WHERE id = 192174;
