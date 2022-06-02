42013ecc995cfd451188d6b852e66b4c2d8f0ae2
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-06-02 08:07:38 +0200

    Update the db create script with new infos in MLD

--- sql/LoCar_db_create.sql
+++ sql/LoCar_db_create.sql
@@ -2,7 +2,7 @@
  * Projet  : LoCar
  * Title   : db_create
  * Author  : Diogo da Silva Fernandes
- * Version : 1.0
+ * Version : 1.1
  */
 
 /* Delete the database if it already exist */
@@ -25,23 +25,26 @@ CREATE TABLE users(
 /* Create offers table */
 CREATE TABLE offers(
     id INT NOT NULL AUTO_INCREMENT,
-    offerNumber INT NOT NULL,
-    title NVARCHAR(30) NOT NULL,
-    description NVARCHAR(300) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    description VARCHAR(300) NOT NULL,
     price DECIMAL(7,2) NOT NULL,
     town VARCHAR(25) NOT NULL,
     brand VARCHAR(20) NOT NULL,
     year YEAR NOT NULL,
     image VARCHAR(100),
     user_id INT NOT NULL,
-    CONSTRAINT PK_User PRIMARY KEY (id),
+    CONSTRAINT PK_Offer PRIMARY KEY (id),
     CONSTRAINT FK_Users_Offers FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT UniqueOffer UNIQUE (offerNumber)
+    CONSTRAINT UniqueOffer UNIQUE (title,user_id)
 );
+SHOW WARNINGS;
+
+/* Update offers table to set AUTO_INCREMENT start to 192168 */
+ALTER TABLE offers AUTO_INCREMENT = 192168;
 
 /* Delete the user if he already exists */
-DROP USER IF EXISTS 'visitor'@'127.0.0.1';
+DROP USER IF EXISTS 'db_master'@'127.0.0.1';
 
 /* Create the user */
-CREATE USER 'visitor'@'127.0.0.1' IDENTIFIED BY 'Pa$$w0rd';
-GRANT SELECT ON locar . * TO 'visitor'@'127.0.0.1';
+CREATE USER 'db_master'@'127.0.0.1' IDENTIFIED BY 'Pa$$w0rd';
+GRANT SELECT, INSERT, UPDATE ON locar . * TO 'db_master'@'127.0.0.1';
