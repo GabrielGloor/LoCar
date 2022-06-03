f7966fac75c49ee55f0a2110a9ca2212f6ffe159
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2022-06-03 08:49:49 +0200

    Update some comments

--- sql/LoCar_db_essentialQueries.sql
+++ sql/LoCar_db_essentialQueries.sql
@@ -2,7 +2,7 @@
  * Projet  : LoCar
  * Title   : db_essentialQueries
  * Author  : Diogo da Silva Fernandes
- * Version : 1.2
+ * Version : 1.3
  */
 
 /* Select the user infos */
@@ -17,7 +17,7 @@ UPDATE users SET username = "diogof647", email = "diogof647@gmail.com", password
 /* Select all offers */
 SELECT id, title, price, town, image FROM offers;
 
-/* Select the offer infos */
+/* Select the offer details */
 SELECT offers.id, offers.title, offers.description, offers.price, offers.town, offers.brand, offers.year, offers.image, users.email FROM users
 INNER JOIN offers ON users.id = offers.user_id
 WHERE offers.id = 192168;
@@ -25,5 +25,5 @@ WHERE offers.id = 192168;
 /* Create a new offer */
 INSERT INTO offers(title, description, price, town, brand, year, image, user_id) VALUES ("Audi A8", "Magnifique audi A8", 192.55, "Yverdon", "Audi", 2003, "img/offer_img/audi_a8.png", 2);
 
-/* Update the offer infos */
+/* Update the offer details */
 UPDATE offers SET title = "Audi A7", description = "Magnifique audi A7", price = 92.55, town = "Yverdon-Les-Bains", brand = "AAudi", year = 2004, image = "img/offer_img/audi_a7.png", user_id = 2 WHERE id = 192174;
